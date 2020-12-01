cbc99323090bf50091f0dd19cec7f0664a4c464e
Ivo C.S. Wingelaar <ivo.wingelaar@gmail.com>
2020-12-01 21:39:17 +0100

    Use a `Counter` to properly handle multisets

--- 1/answer.py
+++ 1/answer.py
@@ -1,27 +1,43 @@
+from collections import Counter
+
 expenses = open('input.txt')
 
-values = set()
+values = Counter()
+needle = 2020
 
 for i in expenses.readlines():
+    # strip newline
     value = int(i[:-1])
-    values.add(value)
+    values.update([value])
 
 
 def find_pair_that_sums_to(values, expected_sum):
     for i in values:
         if (expected_sum - i) in values:
-            return i, expected_sum - i
+            if not is_false_positive(values, [i, expected_sum - i]):
+                return i, expected_sum - i
+
+
+def is_false_positive(values, possible_answer):
+    c = Counter(possible_answer)
+    for k, v in c.items():
+        if values[k] < v:
+            return True
+
+    return False
 
 
 # Part one
-a, b = find_pair_that_sums_to(values, 2020)
+a, b = find_pair_that_sums_to(values, needle)
 print(a * b)
 
 # Part two
 for i in values:
-    res = find_pair_that_sums_to(values, 2020 - i)
+    res = find_pair_that_sums_to(values, needle - i)
 
     if res is not None:
         a, b = res
-        print(a * b * i)
-        break
+
+        if not is_false_positive(values, [a, b, i]):
+            print(a * b * i)
+            break
